8dd56856d7326412233eb05206a96a8da889a1fd
Nazari <nazari-adamenko@yandex.ru>
2022-02-07 23:46:54 +0300

    HTML, CSS меню, спрайты

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,6 +1,12 @@
-// Подключение функционала "Чертогов Фрилансера"
+// Подключение функционала "ЧФ"
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
 
+function sum(a, b){
+    d = a + b;
+    console.log(d);
+}
+
+sum(4, 6);
