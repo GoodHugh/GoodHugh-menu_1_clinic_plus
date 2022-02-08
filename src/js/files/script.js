ec0e95be50a616fed460fe9b150f4dd9f811d764
Nazari <nazari-adamenko@yandex.ru>
2022-02-08 11:39:44 +0300

    Правка файлов шаблона

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,11 +2,3 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
-
-
-function sum(a, b){
-    d = a + b;
-    console.log(d);
-}
-
-sum(4, 6);
