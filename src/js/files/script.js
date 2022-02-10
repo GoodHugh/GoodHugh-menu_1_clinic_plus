ade9d33986a6321b8c2045851a58445530b14f25
Nazari <nazari-adamenko@yandex.ru>
2022-02-10 18:35:27 +0300

    js для кликов на десктопах

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,4 +4,19 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
-console.log("Hello");
+window.onload = function() {
+    document.addEventListener("click", documentAction);
+
+    function documentAction(elem){
+        console.log('1');
+        const targetObj = elem.target;
+        if (window.innerWidth > 768 && isMobile.any()) {
+            console.log('2');
+            console.log(elem);
+                document.querySelector('.menu__sub-list').classList.toggle('_activ_sub-list');
+            
+            
+        }
+    }
+
+}
