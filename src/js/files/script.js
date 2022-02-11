8c4b9530f30ef63d3360bab46dc69ba7eda5a301
Nazari <nazari-adamenko@yandex.ru>
2022-02-11 18:43:38 +0300

    события по таргетам JS

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,19 +4,32 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
-window.onload = function() {
+window.onload = function () {
     document.addEventListener("click", documentAction);
 
-    function documentAction(elem){
-        console.log('1');
-        const targetObj = elem.target;
+    function documentAction(elem) {
+        const targetObjMenu = elem.target;
+        const targetObjSeaech = elem.target;
         if (window.innerWidth > 768 && isMobile.any()) {
-            console.log('2');
-            console.log(elem);
-                document.querySelector('.menu__sub-list').classList.toggle('_activ_sub-list');
-            
-            
+            if (targetObjMenu.classList.contains('menu__list-item-button')) {
+                const parentObjMenu = targetObjMenu.parentElement;
+                parentObjMenu.querySelector('.menu__sub-list').classList.toggle('_menu_sub-list_active');
+                parentObjMenu.querySelector('.menu__list-icon').classList.toggle('_menu_list-icon_actice');
+            }
+            if (!targetObjMenu.classList.contains('menu__list-item-button') && document.querySelectorAll('._menu_sub-list_active').length > 0) {
+                document.querySelectorAll('._menu_sub-list_active').forEach((elem)=>{
+                    elem.classList.remove('_menu_sub-list_active');
+                })
+                document.querySelectorAll('._menu_list-icon_actice').forEach((elem)=>{
+                    elem.classList.remove('_menu_list-icon_actice');
+                })
+            }
+            if (targetObjSeaech.classList.contains('header__top-content-search-box')) {
+                document.querySelector('.header__top-content-search-input').classList.toggle('_header_top-content-search-input_active');
+            }
+            if (!targetObjSeaech.classList.contains('header__top-content-search-box')){
+                document.querySelector('.header__top-content-search-input').classList.remove('_header_top-content-search-input_active');
+            }
         }
     }
-
 }
